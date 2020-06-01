631843433485e0c0f6acdcd89c720e9feeb7913b
Manuel Montenegro <mmg@lcc.uma.es>
2020-06-01 20:52:47 +0000

    Added netstats and blockexplorer

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose --file nodes-docker-compose.yml up --build
-# docker-compose --file netstatsserver-docker-compose.yml up --build -d
-# sleep 20s
-# docker-compose --file netstatsclient-docker-compose.yml up --build -d
+docker-compose --file nodes-docker-compose.yml up --build -d
+docker-compose --file netstatsserver-docker-compose.yml up --build -d
+sleep 20s
+docker-compose --file netstatsclient-docker-compose.yml up --build -d
